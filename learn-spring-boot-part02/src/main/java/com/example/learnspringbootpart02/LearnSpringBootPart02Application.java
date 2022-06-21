292a6ecbd65be99828c4514bbacb393abe9066a0
songk1992-home <zuppiy@naver.com>
2022-06-21 23:12:28 +0900

    JDBCTemplate CRUD

--- learn-spring-boot-part02/src/main/java/com/example/learnspringbootpart02/LearnSpringBootPart02Application.java
+++ learn-spring-boot-part02/src/main/java/com/example/learnspringbootpart02/LearnSpringBootPart02Application.java
@@ -1,11 +1,14 @@
 package com.example.learnspringbootpart02;
 
+import com.example.learnspringbootpart02.entity.Person;
 import com.example.learnspringbootpart02.jdbc.PersonJdbcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class LearnSpringBootPart02Application  implements CommandLineRunner {
@@ -26,6 +29,10 @@ public class LearnSpringBootPart02Application  implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         logger.info("All users -> {}", dao.findAll());
-
+        logger.info("User id 10001 -> {}", dao.findById(10001));
+        logger.info("User name SONG -> {}", dao.findByName("SONG"));
+        logger.info("Delete by id 10002 -> {}", dao.deleteById(10002));
+        logger.info("Insert person -> {}", dao.insertPerson(new Person(10005, "name", "location", new Date())));
+        logger.info("Update person -> {}", dao.updatePerson(new Person(10005, "name2", "location", new Date())));
     }
 }
--- learn-spring-boot-part02/src/main/java/com/example/learnspringbootpart02/jdbc/PersonJdbcDao.java
+++ learn-spring-boot-part02/src/main/java/com/example/learnspringbootpart02/jdbc/PersonJdbcDao.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -19,5 +20,39 @@ public class PersonJdbcDao {
     // select * from person
     public List<Person> findAll() {
         return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    // select one person by id
+    public Person findById(int id) {
+        return jdbcTemplate.queryForObject("select * from person where id=?",
+                new Object[]{id},
+                new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    // select one person by name
+    public Person findByName(String name) {
+        return jdbcTemplate.queryForObject("select * from person where name=?",
+                new Object[]{name},
+                new BeanPropertyRowMapper<>(Person.class));
+    }
+
+    // delete one person by id
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
+    }
+
+    // insert one person
+    public int insertPerson(Person person) {
+        return jdbcTemplate.update("INSERT INTO PERSON\n" +
+                "(ID, NAME, LOCATION, BIRTH_DATE)\n" +
+                "VALUES(?, ?, ?, ?);", new Object[]{person.getId(), person.getName(), person.getLocation(), new Timestamp(person.getBirthdate().getTime())});
+    }
+
+    // update one person
+    public int updatePerson(Person person) {
+        return jdbcTemplate.update("update PERSON " +
+                "set name= ?, location=?, birth_date=? where id= ?",
+                new Object[]{person.getName(), person.getLocation(),
+                        new Timestamp(person.getBirthdate().getTime()), person.getId()});
     }
 }
