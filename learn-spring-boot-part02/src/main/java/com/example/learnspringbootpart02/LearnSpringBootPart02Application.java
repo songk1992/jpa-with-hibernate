ebf267d046173f89001072609858a734373cea8b
songk1992-home <zuppiy@naver.com>
2022-06-21 21:25:22 +0900

    init

---
+++ learn-spring-boot-part02/src/main/java/com/example/learnspringbootpart02/LearnSpringBootPart02Application.java
@@ -0,0 +1,13 @@
+package com.example.learnspringbootpart02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootPart02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootPart02Application.class, args);
+    }
+
+}
