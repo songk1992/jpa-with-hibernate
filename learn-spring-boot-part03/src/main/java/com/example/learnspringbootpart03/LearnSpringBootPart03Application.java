8fa877f93644172fbcd2c260973acce35bd083dd
songk1992-home <zuppiy@naver.com>
2022-06-22 21:03:05 +0900

    init

---
+++ learn-spring-boot-part03/src/main/java/com/example/learnspringbootpart03/LearnSpringBootPart03Application.java
@@ -0,0 +1,13 @@
+package com.example.learnspringbootpart03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootPart03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootPart03Application.class, args);
+    }
+
+}
---
+++ learn-spring-boot-part03/src/test/java/com/example/learnspringbootpart03/LearnSpringBootPart03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnspringbootpart03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootPart03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
