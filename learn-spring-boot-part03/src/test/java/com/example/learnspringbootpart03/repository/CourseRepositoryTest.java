5ad0cfc65be09bb26aec6560654573a71f0a064d
songk1992-home <zuppiy@naver.com>
2022-06-23 20:42:14 +0900

    writing unit test for findbyid

---
+++ learn-spring-boot-part03/src/test/java/com/example/learnspringbootpart03/repository/CourseRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.example.learnspringbootpart03.repository;
+
+import com.example.learnspringbootpart03.LearnSpringBootPart03Application;
+import com.example.learnspringbootpart03.entity.Course;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LearnSpringBootPart03Application.class)
+public class CourseRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CourseRepository repository;
+
+    @Test
+    public void findById(){
+        Course course = repository.findById(10001L);
+        assertEquals("인기강좌", course.getName());
+    }
+}
