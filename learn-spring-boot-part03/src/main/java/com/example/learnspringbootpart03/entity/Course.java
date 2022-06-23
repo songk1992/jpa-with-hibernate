7af30666857b789dacc6276278b1d9d3b2746a56
songk1992-home <zuppiy@naver.com>
2022-06-23 22:26:10 +0900

    @Column

--- learn-spring-boot-part03/src/main/java/com/example/learnspringbootpart03/entity/Course.java
+++ learn-spring-boot-part03/src/main/java/com/example/learnspringbootpart03/entity/Course.java
@@ -1,5 +1,6 @@
 package com.example.learnspringbootpart03.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ public class Course {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
     public Course() {
