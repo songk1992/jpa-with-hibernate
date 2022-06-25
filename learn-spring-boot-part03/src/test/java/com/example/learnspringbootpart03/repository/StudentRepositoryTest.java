1d2979ab53d785077108ce8addca3c9fd980d17b
songk1992-home <zuppiy@naver.com>
2022-06-25 22:13:33 +0900

    OneToOne Mapping - Student 와 Passport EagerFetch

---
+++ learn-spring-boot-part03/src/test/java/com/example/learnspringbootpart03/repository/StudentRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.learnspringbootpart03.repository;
+
+import com.example.learnspringbootpart03.LearnSpringBootPart03Application;
+import com.example.learnspringbootpart03.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LearnSpringBootPart03Application.class)
+class StudentRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    StudentRepository repository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void retrieveStudentAndPasswordDetails() {
+        Student student = em.find(Student.class, 30001L);
+        logger.info("student -> {}", student);
+        logger.info("passport -> {}", student.getPassport());
+    }
+}
