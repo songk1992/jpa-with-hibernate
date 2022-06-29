34a8ad424c99a80039b7fb50232123a7fd1b0e26
songk1992-home <zuppiy@naver.com>
2022-06-29 21:43:10 +0900

    JPQL like

--- learn-spring-boot-part03/src/test/java/com/example/learnspringbootpart03/repository/JPQLTest.java
+++ learn-spring-boot-part03/src/test/java/com/example/learnspringbootpart03/repository/JPQLTest.java
@@ -2,6 +2,7 @@ package com.example.learnspringbootpart03.repository;
 
 import com.example.learnspringbootpart03.LearnSpringBootPart03Application;
 import com.example.learnspringbootpart03.entity.Course;
+import com.example.learnspringbootpart03.entity.Student;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -40,5 +41,26 @@ public class JPQLTest {
         TypedQuery<Course> query = em.createNamedQuery("q_get_like_he_courses", Course.class);
         List<Course> resultList = query.getResultList();
         logger.info("select c from Course c -> {}", resultList);
+    }
+
+    @Test
+    public void jpql_courses_without_students(){
+        TypedQuery<Course> query = em.createQuery("Select c from Course c where c.students is empty", Course.class);
+        List<Course> resultList = query.getResultList();
+        logger.info("Results -> {}", resultList);
+    }
+
+    @Test
+    public void jpql_courses_with_atleast_2_students(){
+        TypedQuery<Course> query = em.createQuery("Select c from Course c where size(c.students) > 2", Course.class);
+        List<Course> resultList = query.getResultList();
+        logger.info("Results -> {}", resultList);
+    }
+
+    @Test
+    public void jpql_student_with_certain_passport(){
+        TypedQuery<Student> query = em.createQuery("Select s from Student s where s.passport.number like '%여권%'", Student.class);
+        List<Student> resultList = query.getResultList();
+        logger.info("Student -> {}", resultList);
     }
 }
