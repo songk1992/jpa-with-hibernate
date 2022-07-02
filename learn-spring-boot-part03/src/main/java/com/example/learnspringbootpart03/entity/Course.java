32d57941344ffffb28aabd12d19b7d9e10ab4c95
songk1992-home <zuppiy@naver.com>
2022-07-02 11:45:59 +0900

    hibernate soft delete

--- learn-spring-boot-part03/src/main/java/com/example/learnspringbootpart03/entity/Course.java
+++ learn-spring-boot-part03/src/main/java/com/example/learnspringbootpart03/entity/Course.java
@@ -1,7 +1,11 @@
 package com.example.learnspringbootpart03.entity;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -17,7 +21,12 @@ import java.util.List;
 
 )
 @Cacheable
+@SQLDelete(sql = "update course set is_deleted=true where id=?")
+@Where(clause = "is_deleted = false")
 public class Course {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(Course.class);
+
     @Id
     @GeneratedValue
     private Long id;
@@ -36,6 +45,14 @@ public class Course {
 
     @ManyToMany(mappedBy = "courses")
     private List<Student> students = new ArrayList<>();
+
+    private boolean isDeleted;
+
+    @PreRemove
+    private void preRemove(){
+        LOGGER.info("Setting isDeleted to True");
+        this.isDeleted = true;
+    }
 
     public void addReviews(Review review) {
         this.reviews.add(review);
--- learn-spring-boot-part03/src/main/resources/data.sql
+++ learn-spring-boot-part03/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into course(id, name,created_date, last_updated_date) values(10001, '인기강좌1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-insert into course(id, name,created_date, last_updated_date) values(10002, '인기강좌2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-insert into course(id, name,created_date, last_updated_date) values(10003, '인기강좌3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+insert into course(id, name,created_date, last_updated_date, is_deleted) values(10001, '인기강좌1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false );
+insert into course(id, name,created_date, last_updated_date, is_deleted) values(10002, '인기강좌2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
+insert into course(id, name,created_date, last_updated_date, is_deleted) values(10003, '인기강좌3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), false);
 
 insert into passport(id, number) values(20001, '여권번호1');
 insert into passport(id, number) values(20002, '여권번호2');
